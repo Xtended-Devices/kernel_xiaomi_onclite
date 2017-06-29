2eecc7906ead2d665e78a8111fdbf895792d075b
Brendan Jackman <brendan.jackman@arm.com>
2017-06-29 17:29:31 +0100

    ANDROID: sched/fair: Sync task util before EAS wakeup
    Before using a task's util_avg signal in EAS, we need to ensure that
    it has been synced up to the last_update_time of prev_cpu's root
    cfs_rq.
    
    We previously relied on the side effect of wake_cap to do that,
    however that does not happen when the waking CPU has the same
    capacity as the prev_cpu. Therefore just explicitly call
    sync_entity_load_avg. This may result in calling that function twice
    within the same select_task_rq_fair, but since last_update_time
    hasn't changed the second call will bail out very quickly.
    
    Change-Id: I91f1fcd71dfeb96b7f5b73418f1cf9ac311d4655
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    (cherry picked from commit 595d8e0d207b9dfb0aa412d24293ed80e6478c88)
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7904,6 +7904,8 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu,
 
 	fbt_env.placement_boost = task_boost_policy(p);
 
+	sync_entity_load_avg(&p->se);
+
 	sd = rcu_dereference(per_cpu(sd_ea, prev_cpu));
 	if (!sd) {
 		target_cpu = prev_cpu;
