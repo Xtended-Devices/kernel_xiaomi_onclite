93ffd57a77f4124f6d5046ecc2eafa60a52c315e
Andrei F <luxneb@gmail.com>
2018-05-25 18:53:39 +0200

    sched/fair: Migration hysteresis filter from HiSilicon EAS
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -116,3 +116,4 @@ SCHED_FEAT(SCHEDTUNE_BOOST_HOLD_ALL, false)
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
 SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
+SCHED_FEAT(HISI_FILTER, true)
