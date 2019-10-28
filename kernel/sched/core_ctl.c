0e9456e99d498b35ba848eca197092927f57f19b
Udit Karode <udit.karode@gmail.com>
2019-10-28 23:46:04 +0000

    sched: core_ctl: add CONFIG_SCHED_WALT check for WALT specific code
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -1198,6 +1198,7 @@ static int __init core_ctl_init(void)
 			"core_ctl/isolation:dead",
 			NULL, core_ctl_isolation_dead_cpu);
 
+#ifdef CONFIG_SCHED_WALT
 	for_each_cpu(cpu, &cpus) {
 		int ret;
 		const struct cpumask *cluster_cpus = cpu_coregroup_mask(cpu);
@@ -1207,6 +1208,8 @@ static int __init core_ctl_init(void)
 			pr_warn("unable to create core ctl group: %d\n", ret);
 		cpumask_andnot(&cpus, &cpus, cluster_cpus);
 	}
+#endif
+
 	initialized = true;
 	return 0;
 }
