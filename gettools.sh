7e9baa2082cddb3dfdd34366507f233d4a71cfd5
mylove90 <engm.saad1990@gmail.com>
2020-02-06 18:20:33 +0200

    Update build tools

--- gettools.sh
+++ gettools.sh
@@ -10,15 +10,15 @@ sudo apt install bc bash git-core gnupg build-essential \
     m4 gcc libtool zlib1g-dev flex bison libssl-dev
 
 # Clone toolchain
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r20 --depth=1 stock
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r20 --depth=1 stock_32
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r29 --depth=1 stock
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r29 --depth=1 stock_32
 
 # Clone AnyKernel3
 git clone https://github.com/mylove90/AnyKernel3
 
 #Download Clang
 if [ ! -d clang ]; then
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-10.0.0_r20/clang-r353983c.tar.gz
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-10.0.0_r29/clang-r353983c.tar.gz
     mkdir -p clang/clang-r353983c/
     tar xvzf clang-r353983c.tar.gz -C clang/clang-r353983c
     rm clang-r353983c.tar.gz
