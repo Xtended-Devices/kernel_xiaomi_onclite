d538fede0da8399275a266f1110478698b19590f
mylove90 <engm.saad1990@gmail.com>
2020-05-02 09:45:10 +0200

    Revert "drm: msm: Add support for display modes."
    This reverts commit a639a01680c1dd523709f5f63856b68e348454dd.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -161,13 +161,6 @@ enum dsi_panel_type {
 	DSI_PANEL_TYPE_MAX,
 };
 
-enum dsi_panel_display_mode {
-	DISPLAY_MODE_DEFAULT,
-	DISPLAY_MODE_SRGB,
-	DISPLAY_MODE_DCI_P3,
-	DISPLAY_MODE_WIDE_COLOR
-};
-
 struct dsi_panel {
 	const char *name;
 	enum dsi_panel_type type;
@@ -230,9 +223,6 @@ struct dsi_panel {
 	bool is_hbm_enabled;
 	int op_force_screenfp;
 	bool dim_status;
-
-	enum dsi_panel_display_mode display_mode;
-
 	bool lp11_init;
 	bool ulps_enabled;
 	bool ulps_suspend_enabled;
@@ -326,8 +316,6 @@ int dsi_panel_disable(struct dsi_panel *panel);
 int dsi_panel_unprepare(struct dsi_panel *panel);
 
 int dsi_panel_post_unprepare(struct dsi_panel *panel);
-
-int dsi_panel_apply_display_mode(struct dsi_panel *panel);
 
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl);
 
