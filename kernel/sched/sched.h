64dc6e2e7328062f404453188e91f4de39cc8e56
mylove90 <engm.saad1990@gmail.com>
2020-06-01 21:19:37 +0200

    Merge tag 'LA.UM.8.6.2.r1-07600-89xx.0' of https://source.codeaurora.org/quic/la/kernel/msm-4.9 into HEAD
    "LA.UM.8.6.2.r1-07600-89xx.0"

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2554,11 +2554,12 @@ DECLARE_PER_CPU(struct update_util_data *, cpufreq_update_util_data);
 static inline void cpufreq_update_util(struct rq *rq, unsigned int flags)
 {
 	struct update_util_data *data;
+	u64 clock;
 
 	data = rcu_dereference_sched(*per_cpu_ptr(&cpufreq_update_util_data,
 						  cpu_of(rq)));
 	if (data)
-		data->func(data, sched_ktime_clock(), flags);
+		data->func(data, clock, flags);
 }
 #else
 static inline void cpufreq_update_util(struct rq *rq, unsigned int flags) {}
